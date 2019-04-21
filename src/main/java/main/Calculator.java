5fcf02c500ea2ac349c216c398b8f998331853ff
DELL-PC <DELL-PC@DELL>
2019-04-21 20:51:31 +0530

    commit and push

--- src/main/java/main/Calculator.java
+++ src/main/java/main/Calculator.java
@@ -39,25 +39,32 @@ public class Calculator {
     {
     	int result=0;
 //        write your code here 
-    	
+    	 
+     	result = x + y;
         return result;
     }
     public static int sub(int x, int y)
     {
     	int result=0;
 //        write your code here
+        
+    	result= x-y;
         return result;
     }
     public static int mult(int x, int y)
     {
     	int result=0;
-//      write your code here        
+//      write your code here 
+    	 
+     	result= x*y;
     	return result;
     }
     public static int div(int x, int y)
     {
     	int result=0;
 //      write your code here
+    	 
+     	result= x/y;
     	return result;
     }
 
